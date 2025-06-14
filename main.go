f4749a1bc553bc06cc9e3b0cae59ec4794e8e3f6
WaldemarEnns <ennsdesigns96@gmail.com>
2025-06-14 11:14:35 +0200

    Adds simple mux server with graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+type ServerConfig struct {
+	Port         string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	Handler      http.Handler
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	s := NewServer()
+
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      s,
+	}
+
+	go StartServer(httpServer)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		log.Default().Println("Shutting down server")
+		shutdownCtx := context.Background()
+		shutdownCtx, cancel := context.WithTimeout(shutdownCtx, 10*time.Second)
+		defer cancel()
+		if err := httpServer.Shutdown(shutdownCtx); err != nil {
+			log.Default().Printf("Error shutting down server: %v", err)
+		}
+	}()
+
+	wg.Wait()
+}
+
+func NewServer() http.Handler {
+	mux := http.NewServeMux()
+
+	var handler http.Handler = mux
+
+	RegisterRoutes(mux)
+
+	return handler
+}
+
+func StartServer(server *http.Server) {
+	log.Default().Println("Starting server on port", server.Addr)
+	log.Fatal(server.ListenAndServe())
+}
+
+func RegisterRoutes(mux *http.ServeMux) {
+	helloWorld := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Default().Println("GET /")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	})
+
+	health := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Default().Println("GET /health")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	mux.Handle("/", helloWorld)
+	mux.Handle("/health", health)
 }
